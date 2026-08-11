699c64a2cfcba897b3aecdff57cfe6aad48c95aa
Koci0l <kowinskidarek@gmail.com>
2026-08-11 18:24:11 +0200

    Cut node

--- src/search.h
+++ src/search.h
@@ -1,3 +1,4 @@
+// search.h
 #pragma once
 #include "chess.hpp"
 #include "types.h"
@@ -11,12 +12,13 @@ void updateAccumulatorForMove(AccumulatorStack& accStack, chess::Board& board, c
 
 int quiescence(chess::Board& board, int alpha, int beta,
                ThreadInfo& thread, int ply_from_root, SearchStats& stats);
+
 int alphaBeta(chess::Board& board, int depth, int alpha, int beta, int ply_from_root,
               ThreadInfo& thread, const TimeManager* tm, SearchStats& stats, bool allow_null,
               chess::Move previous_move, SearchStack* ss,
-              chess::Move excluded_move = chess::Move());
-              
+              chess::Move excluded_move = chess::Move(), bool cut_node = false);
+
 chess::Move search(chess::Board& board, int max_depth, ThreadInfo& thread, TimeManager& tm,
                    int64_t node_limit = 0, int* score_out = nullptr, uint64_t* nodes_out = nullptr);
-                   
+
 std::vector<chess::Move> extractPV(chess::Board board, int max_depth);
